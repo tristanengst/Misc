56259ed293efdb1a90ede7d08532a03ac3e0496e
Tristan Engst <tristanengst@gmail.com>
2023-04-19 22:18:26 -0700

    Update __init__.py

--- src/Misc/__init__.py
+++ src/Misc/__init__.py
@@ -8,7 +8,6 @@ import os
 device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
 # Turn off WandB console logging, since we don't need it and it breaks TQDM.
-os.environ["WANDB_CONSOLE"] = "off"
 
 # Import remaining code
 from .DatasetUtils import *
